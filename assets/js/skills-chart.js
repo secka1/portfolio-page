799c7f805c2f656164f5e3e5f3ebee5b8071b235
Angela <secka1@bfh.ch>
2025-12-19 21:27:18 +0100

    fix: issues with recipe and use colors from moodboard

--- assets/js/skills-chart.js
+++ assets/js/skills-chart.js
@@ -16,8 +16,8 @@ document.addEventListener("DOMContentLoaded", () => {
         datasets: [{
           label: "Skill Level (0–10)",
           data: [5, 4, 5, 8, 6],
-          backgroundColor: "rgba(23, 143, 73, 0.2)",  
-          borderColor: "rgba(27, 48, 18, 1)",        
+          backgroundColor: "rgba(153, 93, 143, 0.2)",  
+          borderColor: "rgba(44, 28, 41, 1)",        
           borderWidth: 2,
           pointBackgroundColor: "rgba(29, 88, 39, 1)"
         }]
